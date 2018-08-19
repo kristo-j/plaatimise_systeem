6db521a8056b4993112c33dfb83998142406efb6
kristo-j <kristo.j@hotmail.com>
2018-08-20 00:31:34 +0300

    template development

--- themes/hambern-hambern-blank-bootstrap-4/assets/js/scripts.js
+++ themes/hambern-hambern-blank-bootstrap-4/assets/js/scripts.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
     new WOW().init();
     plyr.setup();
+
+    $(document).ready(function(){
+        $('#workdone').carousel({
+            interval: 4000
+        })
+    });
+
+    $(function () {
+        $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+    });
 });
